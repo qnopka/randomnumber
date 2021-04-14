18c612a9c71c55ece90a0753c5d91704c0306804
Попович Екатерина <71285738+qnopka@users.noreply.github.com>
2021-04-14 13:28:51 +0300

    u

--- js/main.js
+++ js/main.js
@@ -8,11 +8,10 @@ countThrow = Number(countThrow);
 
 let numberOfSides = prompt('Введите количество сторон кубика');
 
+document.write(`Вы хотите бросить кубик ${countThrow} раз <br>`);
+document.write(`Количество сторон кубика ${numberOfSides} <br><br>`);
+
 numberOfSides = Number(++numberOfSides);
-
-
-document.write(`Вы хотите бросить кубик ${countThrow} раз <br>`)
-document.write(`Количество сторон кубика ${numberOfSides} <br><br>`)
 
 for (let i=1; i<=countThrow; i++) {
     document.write(getRandomInt(1, numberOfSides) + '<br>');
